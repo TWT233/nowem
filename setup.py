1f1a2301b2fafcedc7d585f5ccf8cae6236c6030
TWT <twt2333@outlook.com>
2021-05-21 21:26:28 +0800

    build: init setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="nowem",
+    version="0.0.1",
+    author="TWT233",
+    author_email="TWT2333@outlook.com",
+    description="API wrapper for princess connect re:dive(tw server)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TWT233/nowem",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
