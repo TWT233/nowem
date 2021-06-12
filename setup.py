1d97d14c3aff4e1d12fc0ca223e2c47057d5a983
TWT <twt2333@outlook.com>
2021-06-12 11:06:09 +0800

    build: v0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="nowem",
-    version="0.0.4",
+    version="0.0.5",
     author="TWT233",
     author_email="TWT2333@outlook.com",
     description="API wrapper for princess connect re:dive(tw server)",
