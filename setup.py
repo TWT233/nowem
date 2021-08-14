4ad1484c227d3df2223509c4d1f71fa047a04191
TWT <twt2333@outlook.com>
2021-08-14 16:16:48 +0800

    build: 0.0.9
    requirements.txt, c u next world

--- setup.py
+++ setup.py
@@ -3,12 +3,9 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
-
 setuptools.setup(
     name="nowem",
-    version="0.0.8",
+    version="0.0.9",
     author="TWT233",
     author_email="TWT2333@outlook.com",
     description="API wrapper for princess connect re:dive(tw server)",
@@ -16,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/TWT233/nowem",
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    install_requires=['requests==2.25.1', 'msgpack>=1.0.1', 'pycryptodomex>=3.9.9'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
