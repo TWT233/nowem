f07d6a8358d2018da4452a74ed9aaa12134c830c
chirowang <chirowang@tencent.com>
2023-03-22 20:43:42 +0800

    general reader for proxy man

--- example/pkg_decoder.py
+++ example/pkg_decoder.py
@@ -1,21 +1,32 @@
 import asyncio
+import base64
 import json
 
-from nowem import PCRClient
+from nowem import PCRClient, playerprefs
+
+
+def read_raw(raw: str):
+    """
+    for requests, the raw is plain binary, so cannot be b64decoded and should be b64encoded
+    for responses, the raw is b64encoded already, thus just need to be encoded
+    """
+    try:
+        base64.b64decode(raw, validate=True)
+    except Exception:
+        return base64.b64encode(bytes.fromhex(''.join(raw.split(' '))))
+    return raw.encode()
 
 
 async def main():
-    c = PCRClient(playerprefs='../data/t1-reader.xml', proxy={}, version='2.8.1')
-    print(f'udid_iv: {c.sec._udid_iv}')
+    c = PCRClient('3.7.0', pp_xml=playerprefs.dec_plist_xml('../data/tw.sonet.princessconnect.plist'))
 
     while True:
-        raw = input('base64>>>')
-        dec, key = c.sec.unpack(raw.encode())
-        print(dec)
-        print(json.dumps(dec))
+        raw = input('input(hex)>>>')
+        dec, key = c.sec.unpack(read_raw(raw))
+        print(f'data: {json.dumps(dec)}')
         print(f'key: {key}')
         if 'viewer_id' in dec:
             print(f'viewer_id: {c.sec.decrypt(dec["viewer_id"].encode())}')
 
 
-asyncio.get_event_loop().run_until_complete(main())
+asyncio.run(main())
